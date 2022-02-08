d615b3383f9bc9e2653bb0361c69aa1898ac50ba
tomme87 <tom.roar@halloverden.no>
2022-02-08 16:19:57 +0100

    ADD: FeatureFlag

---
+++ src/Entity/FeatureFlag.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace HalloVerden\FeatureFlagBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use HalloVerden\EntityUtilsBundle\Interfaces\GenericEntityInterface;
+use HalloVerden\EntityUtilsBundle\Traits\DateTimestampableEntityTrait;
+use HalloVerden\EntityUtilsBundle\Traits\PrimaryAndNonPrimaryIdsTrait;
+use Symfony\Component\Validator\Constraints as Assert;
+use JMS\Serializer\Annotation as Serializer;
+
+/**
+ * Class FeatureFlag
+ *
+ * @package HalloVerden\SymfonyFeatureFlagBundle\Entity
+ *
+ * @ORM\Table(uniqueConstraints={@ORM\UniqueConstraint(columns={"type"})})
+ * @ORM\Entity()
+ * @ORM\InheritanceType("JOINED")
+ * @ORM\DiscriminatorColumn(name="type", type="string")
+ */
+abstract class FeatureFlag implements GenericEntityInterface {
+  use PrimaryAndNonPrimaryIdsTrait;
+  use DateTimestampableEntityTrait;
+
+  /**
+   * @var string
+   *
+   * @ORM\Column(name="name", type="string", nullable=false)
+   */
+  private string $name;
+
+  /**
+   * @var string
+   *
+   * @ORM\Column(name="description", type="text", nullable=false)
+   */
+  private string $description;
+
+  /**
+   * @var bool
+   *
+   * @ORM\Column(name="active", type="boolean", nullable=false)
+   */
+  private bool $active = false;
+
+  /**
+   * @return string
+   */
+  public static abstract function getType(): string;
+
+  /**
+   * @return string
+   */
+  public function getName(): string {
+    return $this->name;
+  }
+
+  /**
+   * @param string $name
+   *
+   * @return FeatureFlag
+   */
+  public function setName(string $name): self {
+    $this->name = $name;
+    return $this;
+  }
+
+  /**
+   * @return string
+   */
+  public function getDescription(): string {
+    return $this->description;
+  }
+
+  /**
+   * @param string $description
+   *
+   * @return FeatureFlag
+   */
+  public function setDescription(string $description): self {
+    $this->description = $description;
+    return $this;
+  }
+
+  /**
+   * @return bool
+   */
+  public function isActive(): bool {
+    return $this->active;
+  }
+
+  /**
+   * @param bool $active
+   *
+   * @return FeatureFlag
+   */
+  public function setActive(bool $active): self {
+    $this->active = $active;
+    return $this;
+  }
+
+}
