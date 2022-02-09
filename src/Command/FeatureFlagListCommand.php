d054440cd65667b666caddcefd6541dc646e5cd3
tomme87 <tom.roar@halloverden.no>
2022-02-09 15:23:09 +0100

    ADD: FeatureFlagListCommand

---
+++ src/Command/FeatureFlagListCommand.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace HalloVerden\FeatureFlagBundle\Command;
+
+use HalloVerden\FeatureFlagBundle\Repository\FeatureFlagRepositoryInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+/**
+ * Class FeatureFlagListCommand
+ *
+ * @package HalloVerden\FeatureFlagBundle\Command
+ */
+class FeatureFlagListCommand extends Command {
+  protected static $defaultName = 'feature-flag:list';
+  protected static $defaultDescription = 'delete feature flag';
+
+  private FeatureFlagRepositoryInterface $featureFlagRepository;
+
+  /**
+   * @param FeatureFlagRepositoryInterface $featureFlagRepository
+   */
+  public function __construct(FeatureFlagRepositoryInterface $featureFlagRepository) {
+    $this->featureFlagRepository = $featureFlagRepository;
+
+    parent::__construct();
+  }
+
+  /**
+   * @inheritDoc
+   */
+  protected function configure() {
+    $this->setDefinition([
+      new InputOption('horizontal', null, InputOption::VALUE_NONE, 'Output table horizontal')
+    ]);
+  }
+
+  /**
+   * @inheritDoc
+   */
+  protected function execute(InputInterface $input, OutputInterface $output): int {
+    $io = new SymfonyStyle($input, $output);
+
+    $featureFlags = $this->featureFlagRepository->getAll();
+
+    $table = $io->createTable()->setHeaders(['type', 'name', 'description', 'active']);
+    foreach ($featureFlags as $featureFlag) {
+      $table->addRow([$featureFlag::getType(), $featureFlag->getName(), $featureFlag->getDescription(), $featureFlag->isActive() ? 'yes' : 'no']);
+    }
+    $table->setHorizontal($input->getOption('horizontal'));
+    $table->render();
+    $io->newLine();
+
+    return Command::SUCCESS;
+  }
+
+
+}
